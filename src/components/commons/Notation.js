38b98d587ef265cb49d4900f94e443966e8dcbcd
Corentin Clichy <cor.clichy@gmail.com>
2021-07-16 14:12:08 -0400

    added notation logic

--- src/components/commons/Notation.js
+++ src/components/commons/Notation.js
@@ -1,55 +1,22 @@
 import React, { Component } from "react";
 import "../../style/notation.scss";
+import NotationIcon from "./NotationIcon";
 
 export class Notation extends Component {
   render() {
+    const { rating } = this.props;
+    console.log(rating);
 
-    console.log(this.props);
     return (
       <div className="notation">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-          className="fill"
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-          className="fill"
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-          className="fill"
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-        </svg>
+        {[1, 2, 3, 4, 5].map((index) => (
+          <NotationIcon
+            key={index}
+            index={index}
+            rating={rating}
+            fill={rating >= index ? "fill" : ""}
+          />
+        ))}
       </div>
     );
   }
